2c3198b017ea14147f79f20ffd833b7f64f7c203
Suman Paik <reachout.suman@gmail.com>
2025-01-01 09:44:14 +0530

    browserroute to hashroot

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+// import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route } from 'react-router-dom';
+
 import HomePage from './pages/HomePage';
 import BlogDetails from './pages/BlogDetails';
 
